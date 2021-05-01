cf624b793c44fbc9a5c0fdb90bf0109e777c88e0
Lucia Skalinova <skalinovalucia@gmail.com>
2021-05-01 12:35:51 +0200

    tracitko objednat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navElm = document.querySelector('nav');
+const navAElm = document.querySelectorAll('nav a');
+console.log(navAElm)
+
+
+const navClosed = () => {
+ navElm.classList.toggle('nav-closed');
+} 
+
+const navBtnElm = document.querySelector('#nav-btn');
+navBtnElm.addEventListener('click', navClosed);
+
+navAElm.forEach((elm) => {
+  elm.addEventListener('click', navClosed)
+})
+
+let ordered = true
+
+const orderBtnElm = document.querySelector('.order-btn');
+orderBtnElm.addEventListener('click', () => {
+  if (ordered === true) {
+  orderBtnElm.textContent = 'Zrušit';
+  document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+  ordered = false
+  } else {
+    orderBtnElm.textContent = 'Objednat';
+    document.querySelector('.drink__cup').classList.remove('drink__cup--selected');
+    ordered = true
+  }
+})
+
